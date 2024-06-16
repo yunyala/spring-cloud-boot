184bd44e931555db9cc3a74f3ebe0addda222cb7
yunyala <892722202@qq.com>
2024-06-16 16:45:02 +0800

    增加几个基础模块

---
+++ point-service/src/test/java/com/yunya/pointservice/PointServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunya.pointservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
