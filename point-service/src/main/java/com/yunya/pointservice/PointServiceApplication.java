6bf95e16dd5d0821da82804b9331f6061f39f0de
yunyala <892722202@qq.com>
2024-06-17 14:08:02 +0800

    订单服务和积分服务模块引入nacos服务配置功能

--- point-service/src/main/java/com/yunya/pointservice/PointServiceApplication.java
+++ point-service/src/main/java/com/yunya/pointservice/PointServiceApplication.java
@@ -2,6 +2,7 @@ package com.yunya.pointservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
 public class PointServiceApplication {
--- point-service/src/main/java/com/yunya/pointservice/controller/PointController.java
+++ point-service/src/main/java/com/yunya/pointservice/controller/PointController.java
@@ -1,17 +1,29 @@
 package com.yunya.pointservice.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/point")
+@RefreshScope
 public class PointController {
+
+    @Value("${config.info}")
+    private String configInfo;
 
     @GetMapping(value = "/test")
     public String test() {
         System.out.println("point-service PointController test function...");
         return "point-service PointController test function...";
+    }
+
+    @GetMapping(value = "/testGetConfigInfo")
+    public String testGetConfigInfo() {
+        System.out.println("testGetConfigInfo function...");
+        return "testGetConfigInfo function...{}" + configInfo;
     }
 
 }
